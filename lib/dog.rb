7c9b12081034a0182ba6e14ee2d4396cb303eb10
Ernest <ernest.okiror@student.moringaschool.com>
2023-05-23 01:56:55 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    attr_accessor :name, :breed
+  
+    def initialize
+      @name = ''
+      @breed = ''
+    end
+  end
+class Person
+    attr_accessor :name, :job
+    def person 
+        @name = ''
+        @job  = ''
+    end
 end
+dog = Dog.new
